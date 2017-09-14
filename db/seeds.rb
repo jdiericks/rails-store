87acea6192a2416f150a69145f856f52450f5d1e
JonLuke Diericks <jdiericks@sogomarketingagency.com>
2017-09-14 14:52:45 +0000

    Initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Movie.create(title: 'Batman', release_year: 1992, price: 10.30, description: 'In a world world where the joker dosent exist...', imdb_id: 11245, poster_url: 'http://via.placeholder.com/150x350')
